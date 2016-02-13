58e431ae1842c114383337b4b55d58b590f92b08
Galen Warren <galen@cvillewarrens.com>
2016-02-13 11:44:49 -0500

    initial commit

---
+++ build/tasks/build.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const plumber = require("gulp-plumber")
+const sourceMaps = require("gulp-sourcemaps")
+const changed = require("gulp-changed");
+const notify = require("gulp-notify");
+const config = require("../config")
+
+gulp.task("build", ["clean"], () => {
+	return gulp.src( config.paths.src )
+		.pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+		.pipe(changed( config.paths.distRoot ))
+    .pipe(sourceMaps.init())
+		.pipe(babel({
+			//presets: ["es2015"]
+		}))
+    .pipe(sourceMaps.write("."))
+		.pipe(gulp.dest(config.paths.distRoot));
+});
---
+++ test/test.spec.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var Test = require("../src/test").Test;
+
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+
+      var test = new Test();
+
+      //assert.equal( 1, )
+      assert.equal(1, test.test());
+
+    });
+  });
+})
